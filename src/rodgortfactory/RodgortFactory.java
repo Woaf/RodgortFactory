7008871ef637da9131809611689489ff5dc93786
Woaf <bfazekas@inf.elte.hu>
2018-04-24 19:11:45 +0200

    Implemented runnable methods

--- src/rodgortfactory/RodgortFactory.java
+++ src/rodgortfactory/RodgortFactory.java
@@ -5,7 +5,9 @@
  */
 package rodgortfactory;
 
+import abstract_definitions.GuildMember;
 import guildmembers.GuildMaster;
+import java.util.Random;
 
 /**
  *
@@ -19,16 +21,23 @@ public class RodgortFactory {
     public static void main(String[] args) {
         
         GuildMaster gm = GuildMaster.getInstance();
-        gm.fillInventory();
+        gm.fillBank();
         
-        //Random rand = new Random();
-        //gm.getListOfBaseMaterials().forEach((material) -> {
-        //    int inc = rand.nextInt(20) + 1;
-        //    gm.addToInventory(material, inc);
-        //});
+        Random rand = new Random();
+        gm.getListOfBaseMaterials().forEach((material) -> {
+            int inc = rand.nextInt(20) + 1;
+            gm.addToBank(material, inc);
+        });
         
-        //GuildMember robot = new GuildMember("Chera Fox", gm.bank);
-        //System.out.println(robot.toString());
+        
+        Thread gmRunner = new Thread(gm);
+        gmRunner.start();
+        
+        GuildMember robot = new GuildMember("Chera Fox", gm.bank);
+        Thread robotRunner = new Thread(robot);
+        
+        robotRunner.start();
+        System.out.println(robot.toString());
         
         //GuildMember member = new GuildMember("WoafTheWolf", bank);
         //member.run();
